1481c277dd14f4abb95fc2a097b6d4d3766b9571
Trung Đức <ducltph46032@fpt.edu.vn>
2024-03-13 00:04:00 +0700

    trungduc

---
+++ Admin/Danh_Muc/list.php
@@ -0,0 +1,45 @@
+<div class="container-xxl flex-grow-1 container-p-y">
+    <h4 class="fw-bold py-3 "><span class="text-muted fw-light">Danh mục /</span> Danh sách danh mục</h4>
+    <div class="d-flex align-content-center justify-content-end flex-wrap gap-3 mb-3">
+    <a href="index.php?act=create_danh_muc" class="btn btn-primary text-end">
+    Thêm Danh Mục
+    </a>
+            </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <table class="table text-center">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Tên Danh Mục</th>
+                            <th>Thao Tác</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($listdm as $danhmuc) :
+                            $xoa = "index.php?act=delete_dm&id=" . $danhmuc['id'];
+                            $sua = "index.php?act=update_dm&id=" . $danhmuc['id']; ?>
+                            <tr>
+                                <td><?= $danhmuc['id'] ?></td>
+                                <td><?= $danhmuc['tendm'] ?></td>
+                               <td>
+                                    <div class="change d-flex justify-content-center">
+                                        <form action="<?= $sua ?>" method="post">
+                                            <input type="submit" name="sua-category" class="btn btn-warning  me-2" value="sửa">
+                                        </form>
+                                        <form action="<?= $xoa ?>" method="post">
+                                            <input type="submit" name="" class="btn btn-danger" value="xóa">
+                                        </form>
+                                    </div>
+                                </td>
+                            </tr>
+                        <?php endforeach ?>
+                    </tbody>
+                </table>
+
+            </div>
+        </div>
+    </div>
+</div>
---
+++ Admin/footer.php
@@ -0,0 +1,42 @@
+
+<!-- Footer -->
+<footer class="content-footer footer bg-footer-theme">
+  <div class="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
+    <div class="mb-2 mb-md-0">
+    © <script>document.write(new Date().getFullYear()) </script>, made with ❤️ by <a href="https://themeselection.com/" target="_blank" class="footer-link fw-medium">Nhóm 7</a>
+    </div>
+  </div>
+</footer>
+<!-- / Footer -->   
+          <div class="content-backdrop fade"></div>
+        </div>
+        <!-- Content wrapper -->
+      </div>
+      <!-- / Layout page -->
+    </div>
+    <!-- Overlay -->
+    <div class="layout-overlay layout-menu-toggle"></div>
+    <!-- Drag Target Area To SlideIn Menu On Small Screens -->
+    <div class="drag-target"></div>
+    
+  </div>
+  <!-- / Layout wrapper -->
+  
+  <script src="../assets/vendor/libs/jquery/jquery.js"></script>
+  <script src="../assets/vendor/libs/popper/popper.js"></script>
+  <script src="../assets/vendor/js/bootstrap.js"></script>
+  <script src="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+  <script src="../assets/vendor/libs/hammer/hammer.js"></script>
+  <script src="../assets/vendor/libs/i18n/i18n.js"></script>
+  <script src="../assets/vendor/libs/typeahead-js/typeahead.js"></script>
+  <script src="../assets/vendor/js/menu.js"></script>
+  <!-- endbuild -->
+  <!-- Vendors JS -->
+  <script src="../assets/vendor/libs/apex-charts/apexcharts.js"></script>
+  <!-- Main JS -->
+  <script src="../assets/js/main.js"></script>
+  <!-- Page JS -->
+  <script src="../assets/js/app-ecommerce-dashboard.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</body>
+</html>
